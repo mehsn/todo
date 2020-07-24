31f1a16bd38a261456b5f6f85620c2ab1f5a06f6
mehsn <ihsnun@gmail.com>
2020-07-24 14:31:33 +0430

    Updates

---
+++ precache-manifest.b2f16e98bfaa9b1803b2a890240f7c96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a851f6623587d1131db8",
+    "url": "/static/css/main.f79e8ce6.chunk.css"
+  },
+  {
+    "revision": "a851f6623587d1131db8",
+    "url": "/static/js/main.a851f662.chunk.js"
+  },
+  {
+    "revision": "fdae8f6975bbd5bd457d",
+    "url": "/static/js/1.fdae8f69.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e96bc5a0d9b5bb06fea5797d5eb7cd0f",
+    "url": "/index.html"
+  }
+];
